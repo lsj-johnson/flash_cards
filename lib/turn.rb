0e436fed77821a2308cc5b3e210975d1705b95de
Lu Johnson <lucasjohnson0613@gmail.com>
2022-02-18 17:15:03 -0700

    methods for Turn built

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+
+class Turn
+  attr_reader   :guess
+                :card
+
+  def initialize(guess, card)
+    @guess      = guess
+    @Card       = card
+  end
+
+  def guess
+    @guess
+  end
+
+  def card
+    @card
+  end
+
+  def correct?
+    guess == card.answer
+  end
+
+  def feedback
+    if guess == card.answer
+      'Correct!'
+    else
+      'Incorrect.'
+  end
+end
